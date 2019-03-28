5442c81206a62be8b34a4817c6fc2eca43fcc908
jimbits <contactjamesfoley@gmail.com>
2019-03-27 18:41:25 -0600

    updated build tasks

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, watch, task } = require('gulp')
+
+
+//build directory
+
+var buildDirecotry = [
+  'src/index.html',
+  'src/css/**/*.*',
+  'src/img/**/*.*',
+  'src/js/**/*.*'
+];
+
+
+task('netlifybuild', function () {
+  return src(buildDirecotry, { base: './src' })
+    .pipe(dest('build'));
+});
+
+task('deploy', function () {
+  return src(buildDirecotry, { base: './src' })
+    .pipe(dest('dist'));
+});
+
+
+
+exports.netlifybuild = task('netlifybuild');
+exports.build = task('deploy');
+exports.start = task('deploy');
